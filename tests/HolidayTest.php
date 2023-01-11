d3479a458921234bcc7985116f5ee3f9566af2d4
marcus.jian <renyou815@gmail.com>
2023-01-11 16:14:05 +0800

    refactor test

--- tests/HolidayTest.php
+++ tests/HolidayTest.php
@@ -7,23 +7,66 @@ use PHPUnit\Framework\TestCase;
 
 class HolidayTest extends TestCase
 {
+    private $holiday;
+
+    public function setUp()
+    {
+        $this->holiday = new FakeHoliday();
+    }
+
     public function test_today_is_not_xmas()
     {
-        $holiday = new Holiday();
-        $this->assertTrue('Today is not Xmas' === $holiday->sayHello());
+        $this->givenDate('11', '25');
+        $this->sayHelloShouldBe('Today is not Xmas');
     }
 
     public function test_today_is_xmas()
     {
-        $holiday = new FakeHoliday();
-        $this->assertTrue('Merry Xmas' === $holiday->sayHello());
+        $this->givenDate('12', '25');
+        $this->sayHelloShouldBe('Merry Xmas');
+    }
+
+    /**
+     * @param $month
+     * @param $day
+     *
+     * @return void
+     */
+    protected function givenDate($month, $day): void
+    {
+        $datetime = new DateTime();
+        $datetime->setDate('2000', $month, $day);
+        $this->holiday->setDateTime($datetime);
+    }
+
+    /**
+     * @param $expected
+     *
+     * @return void
+     */
+    protected function sayHelloShouldBe($expected): void
+    {
+        $this->assertTrue($expected === $this->holiday->sayHello());
     }
 }
 
 class FakeHoliday extends Holiday
 {
+    private $dateTime;
+
+    /**
+     * @param mixed $dateTime
+     *
+     * @return FakeHoliday
+     */
+    public function setDateTime($dateTime): FakeHoliday
+    {
+        $this->dateTime = $dateTime;
+        return $this;
+    }
+
     protected function getDateTime(): DateTime
     {
-        return new DateTime('2000-12-25');
+        return $this->dateTime;
     }
 }
