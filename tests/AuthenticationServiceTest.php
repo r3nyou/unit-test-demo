545c795901436def38842124534b7dd14df3d041
marcus.jian <renyou815@gmail.com>
2023-01-12 17:52:53 +0800

    refactor test

--- tests/AuthenticationServiceTest.php
+++ tests/AuthenticationServiceTest.php
@@ -6,15 +6,56 @@ use PHPUnit\Framework\TestCase;
 
 class AuthenticationServiceTest extends TestCase
 {
+    private $target;
+    private $stubProfile;
+    private $stubToken;
+
+    public function setUp()
+    {
+        $this->stubProfile = $this->getMockBuilder(IProfile::class)->getMock();
+        $this->stubToken = $this->getMockBuilder(IRsaToken::class)->getMock();
+        $this->target = new AuthenticationService($this->stubProfile, $this->stubToken);
+    }
+
     public function test_is_valid()
     {
-        $stubProfile = $this->getMockBuilder(IProfile::class)->getMock();
-        $stubProfile->method('getPassword')->willReturn('91');
-        $stubToken = $this->getMockBuilder(IRsaToken::class)->getMock();
-        $stubToken->method('getRandom')->willReturn('000000');
+        $this->givenProfile('marcus', '91');
+        $this->givenToken('000000');
 
-        $target = new AuthenticationService($stubProfile, $stubToken);
-        $actual = $target->isValid('marcus', '91000000');
-        $this->assertTrue($actual);
+        $this->shouldBeValid('marcus', '91000000');
+    }
+
+    /**
+     * @param $account
+     * @param $password
+     *
+     * @return void
+     */
+    protected function givenProfile($account, $password): void
+    {
+        $this->stubProfile->method('getPassword')
+            ->with($account)
+            ->willReturn($password);
+    }
+
+    /**
+     * @param $randomCode
+     *
+     * @return void
+     */
+    protected function givenToken($randomCode): void
+    {
+        $this->stubToken->method('getRandom')->willReturn($randomCode);
+    }
+
+    /**
+     * @param $account
+     * @param $password
+     *
+     * @return void
+     */
+    protected function shouldBeValid($account, $password): void
+    {
+        $this->assertTrue($this->target->isValid($account, $password));
     }
 }
